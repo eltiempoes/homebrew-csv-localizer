5c06f2a872f65c01cae590e49e04f29ca10f61fc
amartinez <amartinez@pelmorex.com>
2021-04-12 18:19:16 +0200

    update repo url to link eltiempo fork

--- csv-localizer.rb
+++ csv-localizer.rb
@@ -1,7 +1,7 @@
 class CsvLocalizer < Formula
   desc "Convert CSV file to iOS and Android localizable strings"
-  homepage "https://github.com/rogermolas/csv-localizer"
-  url "https://github.com/rogermolas/csv-localizer/archive/v1.2.2.tar.gz"
+  homepage "https://github.com/eltiempoes/csv-localizer"
+  url "https://github.com/eltiempoes/csv-localizer/archive/v1.2.2.tar.gz"
   sha256 "03e89e1b90b5f55b7399be869b88b39e4da3a556ce6dd89e999af188bca790d2"
 
   depends_on "python@3"
